f105bb104ac8deb8e90c413563cb5c94d2b95930
lcardosoe <67930084+lcardosoe@users.noreply.github.com>
2020-09-23 21:30:36 -0500

    Add files via upload

---
+++ redsocial-master/Banner.php
@@ -0,0 +1,8 @@
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100 img-responsive" src="images/foto.png" alt="First slide">
+    </div>
+    
+  </div>
+</div>
