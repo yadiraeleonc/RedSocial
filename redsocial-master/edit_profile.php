42564fe22232af7c9f72b3a17888b0886cf49380
lcardosoe <67930084+lcardosoe@users.noreply.github.com>
2020-09-23 21:29:22 -0500

    Update edit_profile.php

--- redsocial-master/edit_profile.php
+++ redsocial-master/edit_profile.php
@@ -2,67 +2,152 @@
 <?php include('session.php'); ?>    
     <body>
 	<?php include('navbar.php'); ?>
-			<div id="masthead">  
-				<div class="container">
-					    <div class="row">
-      <div class="col-md-2">
-		<hr>
-		<center><img class="pp" src="<?php echo $image; ?>" height="140" width="160"></center>
-		<hr>
-		<button class="btn btn-success">Cambiar Foto de Perfil</button>
-      </div>
-		<div class="col-md-10">
-			<?php
-	$query = $conn->query("select * from members where member_id = '$session_id'");
-	$row = $query->fetch();
-	$id = $row['member_id'];
-	?>
-	<hr>
-					<form method="post" action="save_edit.php">
-	<input type="hidden" name="member_id" value="<?php echo $id; ?>">
-	Usuario:<input type="text" name="username" value="<?php echo $row['username']; ?>">
-	<hr>
-	Nombre:<input type="text" name="firstname" value="<?php echo $row['firstname']; ?>">
-	<hr>
-	Apellido:<input type="text" name="lastname" value="<?php echo $row['lastname']; ?>">
-	<hr>
-	Género:
-	<select name="gender">
-		<option><?php echo $row['gender']; ?></option>
-		<option>Hombre</option>
-		<option>Mujer</option>
-	</select>
-	<hr>
-	Fecha de Nacimiento:<input name="birthdate" type="text" value="<?php echo $row['birthdate']; ?>">
-	<hr>
-	Dirección:<input name="address" type="text" value="<?php echo $row['address']; ?>">
-	<hr>
-	Estado:<input name="status" type="text" value="<?php echo $row['status']; ?>">
-	<hr>
-	Móvil:<input name="mobile" type="text" value="<?php echo $row['mobile']; ?>">
-	<hr>
-	Trabajo:<input name="work" type="text" value="<?php echo $row['work']; ?>">
-	<hr>
-	Religión:<input name="religion" type="text" value="<?php echo $row['religion']; ?>">
-	<hr>
-	<br>
-			<center>
-			<button name="save" class="btn edit">Guardar</button>
-			</center>
-	<br>
-	<form>
-		</div>
 
-    </div> 
-				</div><!-- /cont -->
-				<div class="container">
+	<?php include('Banner.php'); ?>
+
+<div id="masthead">  
+	<div class="container">
+		
+		<div class="row">
+	      
+	      <div class="col-md-2 col-sm-12">
+				<hr>
+				<center><img class="pp" src="<?php echo $image; ?>" height="140" width="160"></center>
+				<hr>
+				<button class="btn btn-success">Cambiar Foto de Perfil</button>
+	      </div>
+		  <div class="col-md-10 col-sm-12">
+				
+				<?php
+					$query = $conn->query("select * from members where member_id = '$session_id'");
+					$row = $query->fetch();
+					$id = $row['member_id'];
+				?>
+					<hr>
+									
+
+				<form method="post" action="save_edit.php">
+
 					<div class="row">
-					<div class="col-md-12">
-						<div class="top-spacer"> </div>
+					  	
+							<input  type="hidden" name="member_id" value="<?php echo $id; ?>">
+
+						  <div class="form-group col-md-3">
+					          
+					        <label for="EditarUsuario" >Usuario</label>  
+
+							<input readonly class="form-control" type="text" id="EditarUsuario" name="username" value="<?php echo $row['username']; ?>">
+						  </div>
+
+						  <div class="form-group col-md-3">
+					          
+					        <label for="editarNombre">Nombre</label> 
+							
+							<input class="form-control" id="editar" type="text" name="firstname" value="<?php echo $row['firstname']; ?>">
+
+					      </div> 
+
+					      <div class="form-group col-md-3">
+					          
+					        <label for="editarApellido">Apellido</label> 
+							
+						    <input class="form-control" id="editarApellido" type="text" name="lastname" value="<?php echo $row['lastname']; ?>">
+
+					      </div> 
+
+					      <div class="form-group col-md-3">
+					          
+					        <label for="editarGenero">Género</label> 
+							
+							<select name="gender" class="form-control" id="editarGenero">
+								<option><?php echo $row['gender']; ?></option>
+								<option>Hombre</option>
+								<option>Mujer</option>
+							</select>
+						    
+					      </div>
+
 					</div>
-					</div> 
-				</div><!-- /cont -->
-			</div>
+					<br>
+					<div class="row">      
+
+					      <div class="form-group col-md-3">
+					          
+					        <label for="editarFecha">Fecha de Nacimiento</label> 
+							
+						    <input class="form-control" id="editarFecha" name="birthdate" type="text" value="<?php echo $row['birthdate']; ?>">
+
+					      </div>  
+
+					      <div class="form-group col-md-3">
+					          
+					        <label for="editarDirrecion">Dirección</label> 
+							
+						     <input class="form-control" id="editarDirrecion" name="address" type="text" value="<?php echo $row['address']; ?>">
+						    
+					      </div>
+
+					      <div class="form-group col-md-3">
+					          
+					        <label for="editarEstado">Estado</label> 
+							
+						     <input class="form-control" id="editarEstado" name="status" type="text" value="<?php echo $row['status']; ?>">
+						    
+					      </div>
+
+					      <div class="form-group col-md-3">
+					          
+					        <label for="editarMovil">Móvil</label> 
+							
+						     <input class="form-control" id="editarMovil" name="mobile" type="text" value="<?php echo $row['mobile']; ?>">
+						    
+					      </div>
+
+					</div>
+					 <br>
+					<div class="row">       
+
+					      <div class="form-group col-md-3">
+					          
+					        <label for="editarTrabajo">Trabajo</label> 
+							
+						     <input class="form-control" id="editarTrabajo" name="work" type="text" value="<?php echo $row['work']; ?>">
+						    
+					      </div>
+
+					      <div class="form-group col-md-3">
+					          
+					        <label for="editarRegion">Religión</label> 
+							
+						     <input class="form-control" id="editarRegion" name="religion" type="text" value="<?php echo $row['religion']; ?>">
+						    
+					      </div>
+
+					  </div>	
+					<br>
+				     	<center>
+							<button name="save" class="btn btn-primary btn edit">Guardar</button>
+						</center>
+					<br>
+				<form>
+
+		   </div>
+
+        </div> 
+	
+	</div><!-- /cont -->
+
+	<div class="container">
+		<div class="row">
+		<div class="col-md-12">
+			<div class="top-spacer">
+
+			 </div>
+		</div>
+		</div> 
+	</div><!-- /cont -->
+
+</div>
 
                                                 
                                                                                 
