71ababc72e2726c820f3d452b27c997c0092d922
CAGARZONRO <71109754+CAGARZONRO@users.noreply.github.com>
2020-09-28 19:20:37 -0500

    Update message.php

--- redsocial-master/message.php
+++ redsocial-master/message.php
@@ -46,11 +46,11 @@
 
 										<div class="container">
 		<div class="row">
-			<div class="col-md-8 col-md-offset-9">
+			<div class="col-md-7 col-md-offset-9">
 				<div class="card">
 					<div class="card-body d-flex justify-content-between align-items-left">
 					
-						<a href="#" class="btn btn-primary btn-sm">Crear</a>
+						<a href="#" class="btn btn-primary btn-sm">Enviar</a>
 					<br>
 					<br>
 
@@ -66,7 +66,7 @@
 
 
 
-				<div class="col-md-12 col-sm-3 text-center">
+				<div class="col-md-12 col-sm-3 text-left">
 				<hr>
 				<label>Mensajes:</label>
 				<hr>
@@ -78,11 +78,15 @@
 				
 				?>
 				<div class="mes">
-				<div class="message"><?php echo $row['content']; ?>
+					
+			<div class="col-md-2 col-sm-1 text-center">
+			<img  src="images/fotoperfil.jpg" style="width:70px;height:70px" class="img-circle"></a>
+            </div>
+				<div class="message"><?php echo $row['content'];?>
+					<div class="pull-center">enviado por: 
+					<p class="text-muted"><?php echo $row['firstname']." ".$row['lastname'];?> a las <?php echo $row['date_sended']; ?> <a href="delete_message.php<?php echo '?id='.$id; ?>" class="btn btn-danger"><i class="icon-remove"></i></a></div></p>
 				<hr>
-				<div class="pull-center">enviado por: <?php echo $row['firstname']." ".$row['lastname'];?> a las <?php echo $row['date_sended']; ?></div>
-				<hr>
-				<a href="delete_message.php<?php echo '?id='.$id; ?>" class="btn btn-danger"><i class="icon-remove"></i> Eliminar</a>
+				
 				</div>
 				</div>
 				<?php } ?>
@@ -92,6 +96,15 @@
           <hr>
         </div>
       </div>
+                                                                                       
+	                                                
+                                                      
+   	</div><!--/col-12-->
+  </div>
+</div>
+
+
+
                                                                                        
 	                                                
                                       
