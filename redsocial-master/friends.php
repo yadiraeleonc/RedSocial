cab0695a295bb07ec0512bc98fc405ae6795e28c
CAGARZONRO <71109754+CAGARZONRO@users.noreply.github.com>
2020-09-29 15:53:59 -0500

    Update friends.php

--- redsocial-master/friends.php
+++ redsocial-master/friends.php
@@ -3,8 +3,17 @@
     <body>
 	<?php include('navbar.php'); ?>
 			<div id="masthead">  
+			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100 img-responsive" src="images/foto.png" alt="First slide">
+    </div>
+    
+  </div>
+</div>
 				<div class="container">
 					<?php include('heading.php'); ?>
+					
 				</div><!-- /cont -->
 				<div class="container">
 					<div class="row">
@@ -28,15 +37,13 @@
 <div class="col-md-12">
 <div class="x_panel">
 <div class="x_title">
-<h2>Amigos <small> Encuentra aqui a tus mejores amigos </small></h2>
+<h2>  Amigos <small> Presione en el nombre de su amigo para ir al perfil</small></h2>
 <ul class="nav navbar-right panel_toolbox">
 <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
 </li>
 <li class="dropdown">
 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-<a class="dropdown-item" href="#">Settings 1</a>
-<a class="dropdown-item" href="#">Settings 2</a>
 </div>
 </li>
 <li><a class="close-link"><i class="fa fa-close"></i></a>
@@ -46,10 +53,10 @@
 </div>
 <div class="x_content">
 <div class="row">
-<div class="col-md-4">
+<div class="col-md-2">
 <div class="thumbnail">
 <div class="image view view-first">
-<img style="width: 80%; display: block;" src="images/male5.jpg" alt="image">
+<img style="width: 100%; display: block;" src="images/male5.jpg" alt="image">
 <div class="mask">
 <label><p><a href="#" class="alert-link">Falcao Garcia</a></p></label>
 <div class="tools tools-bottom">
@@ -61,10 +68,10 @@
 </div>
 </div>
 </div>
-<div class="col-md-4">
+<div class="col-md-2">
 <div class="thumbnail">
 <div class="image view view-first">
-<img style="width: 80%; display: block;" src="images/male5.jpg" alt="image">
+<img style="width: 100%; display: block;" src="images/male5.jpg" alt="image">
 <div class="mask" >
 <label><p><a href="#" class="alert-link">Maluma</a></p></label>
 <div class="tools tools-bottom">
@@ -79,10 +86,10 @@
 </div>
 </div>
 </div>
-<div class="col-md-4">
+<div class="col-md-2">
 <div class="thumbnail">
 <div class="image view view-first">
-<img style="width: 80%; display: block;" src="images/male5.jpg" alt="image">
+<img style="width: 100%; display: block;" src="images/male5.jpg" alt="image">
 <div class="mask">
 <label><p><a href="#" class="alert-link">Leonardo</a></p></label>
 <div class="tools tools-bottom">
@@ -97,10 +104,10 @@
 </div>
 </div>
 </div>
-<div class="col-md-4">
+<div class="col-md-2">
 <div class="thumbnail">
 <div class="image view view-first">
-<img style="width: 80%; display: block;" src="images/male5.jpg" alt="image">
+<img style="width: 100%; display: block;" src="images/male5.jpg" alt="image">
 <div class="mask">
 <label><p><a href="#" class="alert-link">El dueño del chuzo</a></p></label>
 <div class="tools tools-bottom">
@@ -111,14 +118,14 @@
 </div>
 </div>
 <div class="caption">
-<p>Ser exitoso no es una opcion</p>
+<p>Ser exitoso</p>
 </div>
 </div>
 </div>
-<div class="col-md-4">
+<div class="col-md-2">
 <div class="thumbnail">
 <div class="image view view-first">
-<img style="width: 80%; display: block;" src="images/male6.jpg" alt="image">
+<img style="width: 100%; display: block;" src="images/male6.jpg" alt="image">
 <div class="mask">
 <label><p><a href="#" class="alert-link">Casi nadie</a></p></label>
 <div class="tools tools-bottom">
@@ -133,6 +140,61 @@
 </div>
 </div>
 </div>
+<div class="col-md-2">
+<div class="thumbnail">
+<div class="image view view-first">
+<img style="width: 100%; display: block;" src="images/male6.jpg" alt="image">
+<div class="mask">
+<label><p><a href="#" class="alert-link">El che</a></p></label>
+<div class="tools tools-bottom">
+<a href="#"><i class="fa fa-link"></i></a>
+<a href="#"><i class="fa fa-pencil"></i></a>
+<a href="#"><i class="fa fa-times"></i></a>
+</div>
+</div>
+</div>
+<div class="caption">
+<p>Revolucion</p>
+</div>
+</div>
+</div>
+<div class="col-md-2">
+<div class="thumbnail">
+<div class="image view view-first">
+<img style="width: 100%; display: block;" src="images/male6.jpg" alt="image">
+<div class="mask">
+<label><p><a href="#" class="alert-link">Don omar</a></p></label>
+<div class="tools tools-bottom">
+<a href="#"><i class="fa fa-link"></i></a>
+<a href="#"><i class="fa fa-pencil"></i></a>
+<a href="#"><i class="fa fa-times"></i></a>
+</div>
+</div>
+</div>
+<div class="caption">
+<p>1 2 3 4</p>
+</div>
+</div>
+</div>
+<div class="col-md-2">
+<div class="thumbnail">
+<div class="image view view-first">
+<img style="width: 75%; display: block;" src="images/fotoperfil.jpg" alt="image">
+<div class="mask">
+<label><p><a href="#" class="alert-link">Ivan Duque</a></p></label>
+<div class="tools tools-bottom">
+<a href="#"><i class="fa fa-link"></i></a>
+<a href="#"><i class="fa fa-pencil"></i></a>
+<a href="#"><i class="fa fa-times"></i></a>
+</div>
+</div>
+</div>
+<div class="caption">
+<p>Hey there</p>
+</div>
+</div>
+</div>
+
 
 </div>
 </div>
